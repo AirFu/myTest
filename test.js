213b97e50dd43a05a465757f7a2a600b4fd570f0
hhFu001 <bgrace5156@gmail.com>
2012-09-13 15:18:25 +0800

    Signed-off-by: hhFu001 <bgrace5156@gmail.com>

--- test.js
+++ test.js
@@ -1,7 +1,159 @@
-var name = 'a';
-(function() {
-    
-    var name = 'b';
-    console.log(this, this.name);
-    
-})(); 
+jQuery172028039938141591847_1342678490549({
+    "total" : 1000,
+    "sortType" : 1,
+    "data" : [{
+        "userID" : 61994400,
+        "title" : "偷心大圣P.S.男01",
+        "itemID" : 50398229,
+        "nickName" : "彩玉橘",
+        "formatPubDate" : "2010-04-27",
+        "picurl" : "http:\/\/i2.tdimg.com\/050\/398\/229\/p.jpg",
+        "playTimes" : 6777664,
+        "formatTotalTime" : "1:14:00",
+        "code" : "4Lsfyiduz20",
+        "datetime" : 1272297600000
+    }, {
+        "userID" : 61994400,
+        "title" : "偷心大圣P.S.男-第21集（大结局)",
+        "itemID" : 55434024,
+        "nickName" : "彩玉橘",
+        "formatPubDate" : "2010-07-19",
+        "picurl" : "http:\/\/i1.tdimg.com\/055\/434\/024\/p.jpg",
+        "playTimes" : 6267327,
+        "formatTotalTime" : "1:19:40",
+        "code" : "v03dtTwA7rw",
+        "datetime" : 1279468800000
+    }, {
+        "userID" : 61994400,
+        "title" : "偷心大圣P.S.男13",
+        "itemID" : 51902240,
+        "nickName" : "彩玉橘",
+        "formatPubDate" : "2010-05-24",
+        "picurl" : "http:\/\/i1.tdimg.com\/051\/902\/240\/p.jpg",
+        "playTimes" : 6042574,
+        "formatTotalTime" : "1:14:06",
+        "code" : "Q_o-aXFi1Vg",
+        "datetime" : 1274630400000
+    }, {
+        "userID" : 61994400,
+        "title" : "偷心大圣P.S.男-第19集",
+        "itemID" : 54506405,
+        "nickName" : "彩玉橘",
+        "formatPubDate" : "2010-07-05",
+        "picurl" : "http:\/\/i2.tdimg.com\/054\/506\/405\/p.jpg",
+        "playTimes" : 5998237,
+        "formatTotalTime" : "1:16:59",
+        "code" : "jbqfDHt9Qjo",
+        "datetime" : 1278259200000
+    }, {
+        "userID" : 61994400,
+        "title" : "偷心大圣P.S.男12",
+        "itemID" : 51554001,
+        "nickName" : "彩玉橘",
+        "formatPubDate" : "2010-05-17",
+        "picurl" : "http:\/\/i2.tdimg.com\/051\/554\/001\/p.jpg",
+        "playTimes" : 5990226,
+        "formatTotalTime" : "1:14:05",
+        "code" : "lNmxb_RB3RA",
+        "datetime" : 1274025600000
+    }, {
+        "userID" : 61994400,
+        "title" : "偷心大圣P.S.男-第16集",
+        "itemID" : 53165192,
+        "nickName" : "彩玉橘",
+        "formatPubDate" : "2010-06-14",
+        "picurl" : "http:\/\/i1.tdimg.com\/053\/165\/192\/p.jpg",
+        "playTimes" : 5961757,
+        "formatTotalTime" : "1:15:08",
+        "code" : "9g9sVsDD1Hg",
+        "datetime" : 1276444800000
+    }, {
+        "userID" : 61994400,
+        "title" : "偷心大圣P.S.男-第15集",
+        "itemID" : 52719283,
+        "nickName" : "彩玉橘",
+        "formatPubDate" : "2010-06-07",
+        "picurl" : "http:\/\/i4.tdimg.com\/052\/719\/283\/p.jpg",
+        "playTimes" : 5957531,
+        "formatTotalTime" : "1:12:30",
+        "code" : "wo_sc140FuM",
+        "datetime" : 1275840000000
+    }, {
+        "userID" : 61994400,
+        "title" : "偷心大圣P.S.男-第17集",
+        "itemID" : 53605113,
+        "nickName" : "彩玉橘",
+        "formatPubDate" : "2010-06-21",
+        "picurl" : "http:\/\/i2.tdimg.com\/053\/605\/113\/p.jpg",
+        "playTimes" : 5954816,
+        "formatTotalTime" : "1:15:00",
+        "code" : "45BooJ4qCsY",
+        "datetime" : 1277049600000
+    }],
+    "offset" : 0,
+    "rows" : 8
+})
+
+jQuery172028039938141591847_1342678490549({
+    "kw" : "爱",
+    "total" : 494,
+    "list" : [{
+        "playUrl" : "http:\/\/www.tudou.com\/albumcover\/O_urB_PKfCU.html",
+        "itemsCount" : 17,
+        "picUrl" : "http:\/\/res.mfs.ykimg.com\/050D00004FB1EBD3979273376A022553",
+        "albumId" : 86978,
+        "name" : "爱无疆 2012-爱无疆",
+        "playTimes" : 46579829
+    }, {
+        "playUrl" : "http:\/\/www.tudou.com\/albumcover\/XMSkxflcOto.html",
+        "itemsCount" : 20,
+        "picUrl" : "http:\/\/res.mfs.ykimg.com\/050D00004FBD92789792737E80001F5A",
+        "albumId" : 88911,
+        "name" : "让爱作主 2012",
+        "playTimes" : 48
+    }, {
+        "playUrl" : "http:\/\/www.tudou.com\/albumcover\/He02cGTcmak.html",
+        "itemsCount" : 0,
+        "picUrl" : "http:\/\/res.mfs.ykimg.com\/050D00004FB3168D0000015BB40772B8",
+        "albumId" : 86783,
+        "name" : "爱的就是你 2012-爱的就是你",
+        "playTimes" : 133083694
+    }, {
+        "playUrl" : "http:\/\/www.tudou.com\/albumcover\/caXJNDNUlj0.html",
+        "itemsCount" : 1,
+        "picUrl" : "http:\/\/i3.tdimg.com\/b\/20120109\/c655.jpg",
+        "albumId" : 70122,
+        "name" : "马格回家",
+        "playTimes" : 68508
+    }, {
+        "playUrl" : "http:\/\/www.tudou.com\/albumcover\/XA_absgC1VM.html",
+        "itemsCount" : 0,
+        "picUrl" : "http:\/\/res.mfs.ykimg.com\/050D00004FBD92A79792737E800624DF",
+        "albumId" : 88825,
+        "name" : "中国爱大歌会 2012",
+        "playTimes" : 399
+    }, {
+        "playUrl" : "http:\/\/www.tudou.com\/albumcover\/bZxjUa4rkW8.html",
+        "itemsCount" : 0,
+        "picUrl" : "http:\/\/i1.tdimg.com\/b\/20120424\/c169.jpg",
+        "albumId" : 75565,
+        "name" : "盛女爱作战-爱作战",
+        "playTimes" : 449169
+    }, {
+        "playUrl" : "http:\/\/www.tudou.com\/albumcover\/lg5cT86L5RY.html",
+        "itemsCount" : 35,
+        "picUrl" : "http:\/\/res.mfs.ykimg.com\/050D00004FB296840000015BB40B7103",
+        "albumId" : 86123,
+        "name" : "我爱我歌 我爱原创",
+        "playTimes" : 113124385
+    }, {
+        "playUrl" : "http:\/\/www.tudou.com\/albumcover\/JoYr77G5YDc.html",
+        "itemsCount" : 1,
+        "picUrl" : "http:\/\/i2.tdimg.com\/b\/20120517\/c632.jpg",
+        "albumId" : 76389,
+        "name" : "极速特警之永远的爱",
+        "playTimes" : 9042
+    }],
+    "offset" : 0,
+    "rows" : 8
+})
